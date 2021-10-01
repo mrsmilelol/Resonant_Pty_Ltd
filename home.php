3f0d1d53bdd3085a01fa441b04b0c3bd1cd0dda9
mary1 <kkog0002@student.monash.edu>
2021-10-01 15:37:24 +1000

    add a title and some buttons to lead to different pages

--- home.php
+++ home.php
@@ -1,1 +1,19 @@
-<?php
+<html>
+<head></head>
+<h1>Welcome!</h1>
+<body>
+<ul class="menu">
+    <li>
+        <a href="index.php">Home</a>
+    </li>
+    <li>
+        <a href="product.php">Product</li>
+    </li>
+    <li>
+        <a href="client.php">Client</a>
+    </li>
+    <li>
+        <a href="logout.php">Log Out</a>
+</ul>
+</body>
+</html>
