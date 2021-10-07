9e2ee08794af3e1f721cc2673b92520a146d482f
ntra0043 <ntra0043@student.monash.edu>
2021-10-08 02:49:02 +1100

    Modifying product and product delete page, and adding product insert page

--- product.php
+++ product.php
@@ -3,16 +3,15 @@
     <title>Product Information</title>
 </head>
 <body>
-<form action="product_search.php" method="post">
+<form action="product_search.php" method="get">
     <input type="text" name="search" placeholder="Search for products..."/>
     <input type="submit" value="Submit" />
 </form>
 <h1>Product Information</h1>
 <button onclick="window.location='product_insert.php'">Add new product information</button>
 <?php
-include("connection.php");
 $dbh = new PDO('mysql:host=localhost;dbname=fit2104_ass2','fit2104','fit2104');
-$query = "SELECT * FROM `product`;";
+$query = "SELECT * FROM `product` INNER JOIN `category` ON product.category_id = category.category_id ORDER BY category_name;";
 $stmt = $dbh->prepare($query);
 if ($stmt->execute()): ?>
     <table border="1">
@@ -22,19 +21,17 @@ if ($stmt->execute()): ?>
             <th>Product Universal Product Code</th>
             <th>Product Price</th>
             <th>Product Category</th>
-            <th>Action</th>>
-            <th>Product Image</th>>
+            <th>Action</th>
         </tr>
         </thead>
         <tbody>
         <?php while ($row = $stmt->fetchObject()): ?>
-            <td><?= $row->product_name ?></td>
             <td><?= $row->product_upc ?></td>
+            <td><?= $row->product_name ?></td>
             <td><?= $row->product_price ?></td>
-            <td><?= $row->product_category ?></td>
-            <td><button id="update" onclick="window.location='product_update.php?product_name=<?= $row->product_name?>'">Update</button>
-                <button id="delete" onclick="window.location='product_delete.php?product_name=<?= $row->product_name?>'">Delete</button></td>
-            <td><button id="upload" onclick="window.location='product_image.php?product_name=<?= $row->product_name?>'">Upload</button></td>
+            <td><?= $row->category_name ?></td>
+            <td><button id="update" onclick="window.location='product_update.php?product_upc=<?= $row->product_upc?>'">Update</button>
+                <button id="delete" onclick="window.location='product_delete.php?product_upc=<?= $row->product_upc?>'">Delete</button></td>
             </tr>
         <?php endwhile; ?>
         </tbody>
