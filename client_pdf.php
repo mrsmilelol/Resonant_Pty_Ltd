bd2e8e7e80f490ac96834fbabdf3d36e16f1ff94
ntra0043 <ntra0043@student.monash.edu>
2021-10-10 18:19:50 +1100

    Proper design for client pdf file

--- client_pdf.php
+++ client_pdf.php
@@ -7,7 +7,7 @@ require("header.php");
 ?>
 <title>Client pdf</title>
 <body>
-<h1>Details of client ID "<?=$_GET['client_id']  ?>" that you are going to print</h1>
+<h3 class="mx-sm-3">Details of client ID "<?=$_GET['client_id']  ?>" that you are going to print</h3>
 <?php
 $dbh = new PDO('mysql:host=localhost;dbname=fit2104_ass2','fit2104','fit2104');
 if (isset($_GET['client_id'])) {
@@ -22,42 +22,45 @@ if (isset($_GET['client_id'])) {
 
 ?>
 <form method="post">
-    <div class="aligned-form">
+    <div class="form-group">
         <div class="row">
-            <label for="client_id">Client ID</label>
-            <input type="number"  id="client_id" name="client_id" value="<?= $_GET['client_id'] ?>" disabled>
+            <label for="client_id" class="mx-sm-3">Client ID</label>
+            <input type="number"  id="client_id" name="client_id" class="form-control-sm mx-sm-4 mb-2 w-25" value="<?= $_GET['client_id'] ?>" disabled>
         </div>
         <div class="row">
-            <label for="client_firstname">Client First Name</label>
-            <input type="text"  id="client_firstname" name="client_firstname" value="<?= $client->client_firstname ?>" disabled>
+            <label for="client_firstname" class="mx-sm-3">Client First Name</label>
+            <input type="text"  id="client_firstname" name="client_firstname" class="form-control-sm mx-sm-4 mb-2 w-25" value="<?= $client->client_firstname ?>" disabled>
         </div>
         <div class="row">
-            <label for="client_lastname">Client Last Name</label>
-            <input type="text"  id="client_lastname" name="client_lastname" value="<?= $client->client_lastname ?>" disabled>
+            <label for="client_lastname" class="mx-sm-3">Client Last Name</label>
+            <input type="text"  id="client_lastname" name="client_lastname" class="form-control-sm mx-sm-4 mb-2 w-25" value="<?= $client->client_lastname ?>" disabled>
         </div>
         <div class="row">
-            <label for="client_address">Client Address</label>
-            <input type="text"  id="client_address" name="client_address" value="<?= $client->client_address ?>" disabled>
+            <label for="client_address" class="mx-sm-3">Client Address</label>
+            <input type="text"  id="client_address" name="client_address" class="form-control-sm mx-sm-4 mb-2 w-25" value="<?= $client->client_address ?>" disabled>
         </div>
         <div class="row">
-            <label for="client_phone">Client Phone</label>
-            <input type="text" id="client_phone" name="client_phone" value="<?= $client->client_phone ?>" disabled>
+            <label for="client_phone" class="mx-sm-3">Client Phone</label>
+            <input type="text" id="client_phone" name="client_phone" class="form-control-sm mx-sm-4 mb-2 w-25" value="<?= $client->client_phone ?>" disabled>
         </div>
         <div class="row">
-            <label for="client_email">Client Email</label>
-            <input type="text" id="client_email" name="client_email" value="<?= $client->client_email ?>" disabled>
+            <label for="client_email" class="mx-sm-3">Client Email</label>
+            <input type="text" id="client_email" name="client_email" class="form-control-sm mx-sm-4 mb-2 w-25" value="<?= $client->client_email ?>" disabled>
         </div>
         <div class="row">
-            <label for="client_subscribed">Client Subscribed</label>
-            <input type="text" id="client_subscribed" name="client_subscribed" value="<?= $client->client_subscribed ?>" disabled>
+            <label for="client_subscribed" class="mx-sm-3">Client Subscribed</label>
+            <input type="text" id="client_subscribed" name="client_subscribed" class="form-control-sm mx-sm-4 mb-2 w-25" value="<?= $client->client_subscribed ?>" disabled>
         </div>
         <div class="row">
-            <label for="client_other_information">Client Other Information</label>
-            <input type="text" id="client_other_information" name="client_other_information" value="<?= $client->client_other_information ?>" disabled>
+            <label for="client_other_information" class="mx-sm-3">Client Other Information</label>
+            <textarea type="text" id="client_other_information" name="client_other_information" class="form-control-sm mx-sm-4 mb-2 w-25" rows="5" value="<?= $client->client_other_information ?>" disabled></textarea>
         </div>
-        <div>
-            <button type="button" onclick="window.location='temp.php?client_id=<?= $_GET['client_id'] ?>'">Print</button>
-            <button type="button" onclick="window.location='client.php'">Back to Client list</button>
+        <div class="row">
+            <button type="button" class="btn btn-outline-success w-25 mx-sm-4 mb-2" onclick="window.location='temp.php?client_id=<?= $_GET['client_id'] ?>'">Print</button>
+
+        </div>
+        <div class="row">
+            <button type="button" class="btn btn-outline-danger w-25 mx-sm-4 mb-2" onclick="window.location='client.php'">Back to Client list</button>
         </div>
     </div>
 </form>
